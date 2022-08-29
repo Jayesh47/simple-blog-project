c05e5f645c13dd73cd9885559b28794a86a9fc3b
Jayesh47 <Jayeshmalviya47@gmail.com>
2022-08-29 17:47:10 +0530

    second commit

--- BlogDesign/urls.py
+++ BlogDesign/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('<str:slug>', views.BlogPosts, name='BlogPosts'),
+    url(r'^media/(?P<path>.*)$', serve, {
+        'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$',serve, {'document_root':settings.STATIC_ROOT}),
 ]
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonlearner.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
